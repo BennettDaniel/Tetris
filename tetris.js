0fd15532ce22118e63edeaf2248af0d9245a3d97
BennettDaniel <39808069+BennettDaniel@users.noreply.github.com>
2018-07-06 16:03:41 -0400

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -8,6 +8,20 @@ const matrix = [
     [0,1,0],
 ];
 
+function collide(arena, player){
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length;y++) {
+        for (let x = 0; x < m[y].length; x++){
+            if (m[y][x] !== 0 &&
+                (arena[y+o.y] &&
+                arena[y+o.y][x+o.x]) !== 0){
+                    return true;
+                }
+        }
+    }
+    return false;
+}
+
 function createMatrix (w,h) {
     const matrix = [];
     while (h--) {
@@ -19,6 +33,7 @@ function createMatrix (w,h) {
 function draw () {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(arena,{x:0,y:0})
     drawMatrix(player.matrix,player.pos);
 }
 
@@ -34,8 +49,23 @@ function drawMatrix(matrix, offset){
         });
 };
 
+function merge(arena,player){
+    player.matrix.forEach(function(row,y) {
+       row.forEach(function (value, x){
+            if (value!==0){
+                arena[y+player.pos.y][x+player.pos.x]=value
+            }
+       });
+    });
+}
+
 function playerDrop() {
     player.pos.y++;
+    if (collide(arena,player)){
+        player.pos.y--;
+        merge(arena,player);
+        player.pos.y=0;
+    }
     dropCounter=0;
 }
 
@@ -58,8 +88,7 @@ function update (time=0) {
 }
 
 const arena = createMatrix (12,20);
-console.log(arena);
-console.table(arena);
+
 
 const player = {
     pos: {x:5, y:5},
@@ -69,11 +98,9 @@ const player = {
 document.addEventListener('keydown', function (event){
     if (event.keyCode === 37){
         player.pos.x--;
-    };
-    if (event.keyCode === 39){
+    } else if (event.keyCode === 39) {
         player.pos.x++;
-    };
-    if (event.keyCode === 40){
+    }  else if (event.keyCode === 40){
         playerDrop();
 
     };
@@ -81,3 +108,5 @@ document.addEventListener('keydown', function (event){
 });
 
 update();
+
+//Ended video at 23:06
